bac43cc67bdaf4b9aed340e447ed151a7b6f9e6d
Yeshwant Thota <thotayeshwant@gmail.com>
2022-10-25 20:27:42 +0530

    portfolio done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Navbar from "./components/Navbar";
 
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
-
+      <About />
+      <Portfolio />
       <SocialLinks />
     </div>
   );
